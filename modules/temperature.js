79f0b2eb447ccc32801594e5e597d4db7bdd97a4
Peter Peerdeman <peter@lifely.nl>
2020-05-19 21:36:26 +0200

    refactor(temperature): rewrite temperature to use injected influxclient plus some improved promise handling

--- modules/temperature.js
+++ modules/temperature.js
@@ -1,34 +1,33 @@
 const axios = require("axios");
 const fs = require('fs');
-const Influx = require('influxdb-nodejs');
-const influxClient = new Influx('http://127.0.0.1:8086/raspweather-outside');
 
 const writeLog = function(temperatureFloat) {
     var logEntry = new Date().toString() + ';' + temperatureFloat + '\n';
     fs.appendFile('temperatures-outside.txt', logEntry, function(err) {
         //
     });
-}
+};
 
-const writeInflux = function(temperatureFloat) {
-    influxClient.write('file').field({
+const writeInflux = function(influxClient, temperatureFloat) {
+    return influxClient.write('file').field({
         time: Date.now(),
         temperature: temperatureFloat,
-    }).then(() => console.debug(`${Date.now()} temperature: influx write point success`))
-    .catch((error) => console.debug(`${Date.now()} temperature: retrieve temperature failed ${error}`));
-}
+    }).then(() => {
+        return console.debug(`${Date.now()} temperature: influx write point success`);
+    });
+};
 
-const logTemperature = async url => {
+const logTemperature = async influxClient => {
     var url = `https://api.particle.io/v1/devices/${process.env.PARTICLE_DEVICE_ID}/temperature?access_token=${process.env.PARTICLE_ACCESS_TOKEN}`;
     try {
         const response = await axios.get(url);
         const temperatureFloat = response.data.result;
         if (parseFloat(temperatureFloat) < -100 || temperatureFloat == "-0.0625" || parseFloat(temperatureFloat) >100) return;
         writeLog(temperatureFloat);
-        writeInflux(temperatureFloat);
+        writeInflux(influxClient, temperatureFloat);
     } catch (error) {
-        console.debug(`${Date.now()} temperature: retrieve temperature failed ${error}`);
+        return console.debug(`${Date.now()} temperature: retrieve temperature failed ${error}`);
     }
-}
+};
 
 module.exports = logTemperature;
