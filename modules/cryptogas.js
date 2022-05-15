4e932f51647a870b073d6a51e6ca235f7ef5a8b4
Peter Peerdeman <peterpeerdeman@gmail.com>
2022-05-15 08:45:26 +0200

    fix(cryptogas): switch to non-deprecated gas source

--- modules/cryptogas.js
+++ modules/cryptogas.js
@@ -1,19 +1,16 @@
 const axios = require('axios');
 
 const writeInflux = async (influxClient, data) => {
-    const {
-        fast,
-        fastest,
-        safeLow,
-        average,
-        block_time,
-        blockNum,
-        speed,
-        safeLowWait,
-        avgWait,
-        fastWait,
-        fastestWait,
-    } = data;
+    const { avgGas, avgTime, avgTx, baseFee, lastBlock } = data;
+
+    const slowPriceWei = data.speeds[0].gasPrice;
+    const standardPriceWei = data.speeds[1].gasPrice;
+    const fastPriceWei = data.speeds[2].gasPrice;
+    const instantPriceWei = data.speeds[3].gasPrice;
+    const slowFeeDollars = data.speeds[0].estimatedFee;
+    const standardFeeDollars = data.speeds[1].estimatedFee;
+    const fastFeeDollars = data.speeds[2].estimatedFee;
+    const instantFeeDollars = data.speeds[3].estimatedFee;
 
     influxClient
         .write('cryptogas')
@@ -21,17 +18,19 @@ const writeInflux = async (influxClient, data) => {
             crypto: 'eth',
         })
         .field({
-            fast,
-            fastest,
-            safeLow,
-            average,
-            block_time,
-            blockNum,
-            speed,
-            safeLowWait,
-            avgWait,
-            fastWait,
-            fastestWait,
+            avgGas,
+            avgTime,
+            avgTx,
+            baseFee,
+            lastBlock,
+            slowPriceWei,
+            standardPriceWei,
+            fastPriceWei,
+            instantPriceWei,
+            slowFeeDollars,
+            standardFeeDollars,
+            fastFeeDollars,
+            instantFeeDollars,
         })
         .then(() => console.debug(`${Date.now()} cryptogas: write success`))
         .catch((err) =>
@@ -42,7 +41,7 @@ const writeInflux = async (influxClient, data) => {
 };
 
 const logCryptoGas = async (influxClient) => {
-    const url = `https://ethgasstation.info/json/ethgasAPI.json`;
+    const url = `https://owlracle.info/eth/gas`;
     try {
         const response = await axios.get(url);
         writeInflux(influxClient, response.data);
